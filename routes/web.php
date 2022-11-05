831b3bed9e4912d67fdb42f50670d2a8ba23d479
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-11-05 11:54:59 +0600

    add route sendNotification and implement

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Events\demoEvent;
+use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+/**
+ *  sent notification using event
+ */
+Route::get('/sendNotification',function (){
+    $user_id=2;
+    demoEvent::dispatch($user_id);
 });
