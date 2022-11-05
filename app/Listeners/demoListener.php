c96d363d383a98d6c619706bc992daa90b2c603d
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-11-05 11:11:17 +0600

    Create listener demoListener.php for demoEvent and implement  handle function for mail sent

---
+++ app/Listeners/demoListener.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\demoEvent;
+use App\Models\User;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Mail;
+
+class demoListener
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  \App\Events\demoEvent  $event
+     * @return void
+     */
+    public function handle(demoEvent $event)
+    {
+        $user = User::find($event->user_id)->toArray();
+        $data=['name'=>$user->name, 'bodyData'=>'Thanks for using our application'];
+        Mail::send('emails.mailEvent',$data, function($message) use ($user) {
+            $message->to($user['email']);
+            $message->subject('Event Testing');
+        });
+    }
+}
